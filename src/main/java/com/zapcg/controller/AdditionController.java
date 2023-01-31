0e6d3b623c9ef742147c03efb581707e14c6c207
Prathyusha  Penmetsa <prathyusha.penmetsa@zapcg.com>
2023-01-31 12:19:09 +0530

    added some addiotn prog

--- src/main/java/com/zapcg/controller/AdditionController.java
+++ src/main/java/com/zapcg/controller/AdditionController.java
@@ -2,4 +2,9 @@ package com.zapcg.controller;
 
 public class AdditionController {
    //added from git 
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("the sum is "+(a+b));
+	}
 }
